718eeb277bb22de7c4309cd87871170971c4790b
Chris Guest <chrisguest75@users.noreply.github.com>
2023-05-08 22:36:34 +0100

    Working towards getting the datagrid loading from an api

--- 08_datagrid/README.md
+++ 08_datagrid/README.md
@@ -9,13 +9,9 @@ nvm use
 
 npm install
 
-# run
+# run and click the hyperlink.
 npm run dev
 ```
-
-Now click the hyperlink.  
- 
-
 
 ## Created
 
@@ -26,12 +22,12 @@ npm install @mui/material @emotion/react @emotion/styled
 npm install @mui/material @mui/styled-engine-sc styled-components
 npm install @mui/icons-material
 npm install @mui/lab @mui/x-data-grid
+
+npm install react-bootstrap bootstrap       
 ```
 
 ## Resources
 
-https://dev.to/suniljoshi19/using-data-grid-component-in-mui-x-with-react-5180
-https://learnjsx.com/category/4/posts/nextjs-materialui-data-grid
-
-	
-How To Setup Material Grid In Next JS With Material UI/MUI Component https://www.c-sharpcorner.com/article/how-to-setup-material-grid-in-next-js-with-material-uimui-component/
+* Using Data Grid Component in MUI X with React [here](https://dev.to/suniljoshi19/using-data-grid-component-in-mui-x-with-react-5180)
+* How to use Material UI (MUI) data grid in Next.js [here](https://learnjsx.com/category/4/posts/nextjs-materialui-data-grid)
+* How To Setup Material Grid In Next JS With Material UI/MUI Component [here](https://www.c-sharpcorner.com/article/how-to-setup-material-grid-in-next-js-with-material-uimui-component/)
--- 08_datagrid/pages/index.tsx
+++ 08_datagrid/pages/index.tsx
@@ -1,21 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { rows, columns } from '../lib/data'
 
 const MaterialGridCompo = () => {
 	return (
 		<div>
-			<Box sx={{ height: 500, width: 900 }}>
+			<Box sx={{ height: 700, width: 1000 }}>
 				<DataGrid
 					rows={rows}
 					columns={columns}
-					pageSize={5}
-					rowsPerPageOptions={[5]}
+					pageSize={20}
+					rowsPerPageOptions={[20]}
 					checkboxSelection
 					disableSelectionOnClick
 					experimentalFeatures={{ newEditingApi: true }}
@@ -26,35 +27,40 @@ const MaterialGridCompo = () => {
 }
 
 export default function Home() {
-  return (
-	<>
-	  <div className={styles.container}>
+	// react Hook For State Handler
+	const [data, setData] = useState(null);
 
-		<h1 className={styles.title}>
-		  Datagrid Demo
-		</h1>
-		<main className={styles.main}>
-		  <MaterialGridCompo />
-		</main>
+	// Fetch Function
+	/*fetch('got.json').then(
+		function (res) {
+			const data = res.json();
+			console.log(data);
+			return data;
+		},
+	).then(function (data) {
+		// store Data in State Data Variable
+		setData(data);
+	}).catch(
+		function (err) {
+			console.log(err, ' error');
+		},
+	);*/
 
-		<footer className={styles.footer}>
-		  <a
-			href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-			target="_blank"
-			rel="noopener noreferrer"
-		  >
-			Powered by{' '}
-			<span className={styles.logo}>
-			  <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-			</span>
-		  </a>
-		</footer>
-	  </div>
-	  <br></br>
-	  <br></br>
-	  <br></br>
-	  <br></br>
-	  <br></br>
-	</>
-  )
+	return (
+		<>
+		<div className={styles.container}>
+
+			<h1 className={styles.title}>
+			Dynamic
+			</h1>
+			<main className={styles.main}>
+			<MaterialGridCompo />
+			</main>
+
+			<footer className={styles.footer}>
+			</footer>
+		</div>
+		<br></br>
+		</>
+	)
 }
